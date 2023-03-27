fabda100923eb5c237a5b668475c2c42e77de4e7
trujillola <trujillola@eisti.eu>
2023-03-27 12:02:18 +0200

    legend

--- SplitLegend.py
+++ SplitLegend.py
@@ -3,8 +3,6 @@ from PIL import Image
 import numpy as np
 import pytesseract
 
-
-
 dir = './NO_Quad_15/15_3-4/'
 
 image = cv2.imread(dir+'legend.png')
@@ -34,24 +32,43 @@ print(text)
 contours, hierarchy = cv2.findContours(blur, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 # Extraire les rectangles de l'image en utilisant les contours trouvés
+boxes = []
 for contour in contours:
     # Calculer l'aire du contour
     area = cv2.contourArea(contour)
    
     # Ignorer les contours trop petits ou trop grands
-    if area < 2000 or area > 80000:
+    if area < 4000 or area > 80000:
          continue
 
-    print(area)  
+    # print(area)  
 
     # Trouver le rectangle qui englobe le contour
     rect = cv2.minAreaRect(contour)
     box = cv2.boxPoints(rect)
     box = box.astype('int')
-    # print(box)
-
+    print(box)
+    boxes.append(box)
+    
     # Dessiner le rectangle sur l'image originale
+
+box_width = boxes[0][0][1] - boxes[0][2][1]
+box_height = boxes[0][2][1] - boxes[0][1][1]
+x_extreme_start = boxes[0][0][0]
+x_extreme_end = boxes[1][0]
+y_extreme_start = boxes[0][1]
+y_extreme_end = boxes[2][1]
+
+for box in boxes : 
+    box[1][0] = box[1][1] + box_width * 3
     cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+
+print(box_width)
+print(box_height)
+print(x_extreme_start)
+print(x_extreme_end)
+print(y_extreme_start)
+print(y_extreme_end)
 
 # Afficher l'image avec les rectangles extraits
 cv2.imshow('Legende avec les rectangles extraits', img)
